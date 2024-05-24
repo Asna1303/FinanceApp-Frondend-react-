8dc10f1926f302f84ef9fe43934e817de7884264
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-24 10:52:15 +0530

    imported api from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
-import api from './api'
 import React, {useState, useEffect} from 'react'
-
+import api from './api'
 const App = () => {
   const [transactions, setTransactions] = useState([]);
   const [formData, setFormData] = useState({
